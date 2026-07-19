beb99ac59d5d2f903c08cd971925712b6a0e6ea0
Anjali Bisht <anjali18102005@gmail.com>
2026-07-19 19:13:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // Without them, the model will fail to load with a cross-origin isolation error.
 export default defineConfig({
   plugins: [react()],
+  base:'/Midnight-hackathon-project-/
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
